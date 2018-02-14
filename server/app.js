1ef1f132662e5c4c155d23ca8a43f47a1d37267b
niroj_magar <youkuikunda@gmail.com>
2018-02-14 14:22:14 +0545

    project restructured to add client side script react

--- server/app.js
+++ server/app.js
@@ -9,24 +9,22 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost/APIAuthentication', { useMongoClient: true });
 // The `useMongoClient` option is no longer necessary in mongoose 5.x, please remove it.
-mongoose.connect('mongodb://localhost/APIAuthentication');
+// mongoose.connect('mongodb://localhost/APIAuthentication');
+if( process.env.MODE_ENV === 'test' ){
+	mongoose.connect('mongodb://localhost/nodeapitest');
+} else {
+	mongoose.connect('mongodb://localhost/nodeapi');
+	app.use(morgan('dev'));
+}
 
 
-
-// Middleware
-app.use(morgan('dev'));
 // app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 
 
 // Routes
 app.use('/users', require('./routes/users'));
 
 
-
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Server listening at ',port);
+module.exports = app;
